63d41fbf696c50dac5c54ea5ac7ae52b31ceea71
J Lannoy <jonathan@zileo.net>
2022-02-16 09:32:07 +0100

    Keycloak dev trial

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
 if [[ -z "${KEYCLOAK_VERSION}" ]]; then
   echo "No KEYCLOAK_VERSION environment variable set"
   exit 1
@@ -10,3 +8,6 @@ fi
 wget https://github.com/keycloak/keycloak/releases/download/${KEYCLOAK_VERSION}/keycloak-${KEYCLOAK_VERSION}.zip
 unzip keycloak-${KEYCLOAK_VERSION}.zip
 mv keycloak-${KEYCLOAK_VERSION} keycloak
+rm keycloak-${KEYCLOAK_VERSION}.zip
+
+#keycloak/bin/kc.sh build
