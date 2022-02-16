d06bde2f181589b1573fe6206e27cdb3e30eba4d
J Lannoy <jonathan@zileo.net>
2022-02-16 14:52:24 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-keycloak/bin/kc.sh start --hostname ${KEYCLOAK_HOSTNAME} --db-url 'jdbc:postgresql://${POSTGRESQL_ADDON_HOST}:${POSTGRESQL_ADDON_PORT}/${POSTGRESQL_ADDON_DB}' --db-username ${POSTGRESQL_ADDON_USER} --db-password ${POSTGRESQL_ADDON_PASSWORD} --proxy edge
+hostname="${KEYCLOAK_HOSTNAME}"
+
+if [[ -z "$hostname" ]]; then
+	hostname="${APP_ID}.cleverapps.io"
+fi
+
+keycloak/bin/kc.sh start --hostname $hostname --db-url 'jdbc:postgresql://${POSTGRESQL_ADDON_HOST}:${POSTGRESQL_ADDON_PORT}/${POSTGRESQL_ADDON_DB}' --db-username ${POSTGRESQL_ADDON_USER} --db-password ${POSTGRESQL_ADDON_PASSWORD} --proxy edge
