4627223ef075ebd8aea3bba6f4c8f21b27196bd6
J Lannoy <jonathan@zileo.net>
2022-05-26 09:31:52 +0200

    export needed vars

--- start.sh
+++ start.sh
@@ -7,13 +7,16 @@ if [[ -z "${KC_HOSTNAME}" ]]; then
 fi
 
 # Check if a postgres or mysql addon is linked to this application, in order to get the URI of database
-if [[ -z "${POSTGRESQL_ADDON_HOST}" ]]; then
-	KC_DB_URL="jdbc:${POSTGRESQL_ADDON_HOST}"
+if [[ -n "${POSTGRESQL_ADDON_URI}" ]]; then
+	export KC_DB_URL="jdbc:postgresql://${POSTGRESQL_ADDON_HOST}:${POSTGRESQL_ADDON_PORT}/${POSTGRESQL_ADDON_DB}"
+	export KC_DB_USERNAME="${POSTGRESQL_ADDON_USER}"
+	export KC_DB_PASSWORD="${POSTGRESQL_ADDON_PASSWORD}"
 fi
 
-if [[ -z "${MYSQL_ADDON_HOST}" ]]; then
-	KC_DB_URL="jdbc:${MYSQL_ADDON_HOST}"
+if [[ -n "${MYSQL_ADDON_URI}" ]]; then
+	export KC_DB_URL="jdbc:mysql://${MYSQL_ADDON_HOST}:${MYSQL_ADDON_PORT}/${MYSQL_ADDON_DB}"
+	export KC_DB_USERNAME="${MYSQL_ADDON_USER}"
+	export KC_DB_PASSWORD="${MYSQL_ADDON_PASSWORD}"
 fi
-
 
 keycloak/bin/kc.sh start --proxy edge
